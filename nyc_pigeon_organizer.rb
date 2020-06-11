b577c6f7a73760c7b5baf8cab322ea20143ef683
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-11 20:24:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,52 @@
 require 'pry'
+# def nyc_pigeon_organizer(data)
+#   # write your code here!
+#    final_results = data.each_with_object({}) do |(key,value), final_array|
+#      #
+#      value.each do |inner_key, names|
+#        names.each do |name|
+#       #binding.pry   
+#       
+#       if !final_array[name]
+#         final_array[name] = {}
+#         end
+#         
+#         if !final_array[name][key]
+#             !final_array[name][key] = []
+#           end
+#           
+#           final_array[name][key].push(inner_key.to_s)
+#      end
+#     end
+#    end
+#   binding.pry
+# end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-   final_results = data.each_with_object({}) do |(key,value), final_array|
-     value.each do |inner_key, names|
-       names.each do |name|
-      #binding.pry   
-      if !final_array[name]
-        final_array[name] = {}
+  new_hash = {}
+  
+  data.each do |key, value|
+    # puts "Data's #{key} is #{value}"
+    
+    value.each do |new_value, names|
+      # puts "Data's #{new_value} is #{names}"
+      names.each do |name|
+        
+        # puts "Data's #{name}"
+        if !new_hash[name]
+          new_hash[name] = {}
         end
-        if !final_array[name][key]
-            !final_array[name][key] = []
-          end
-          final_array[name][key].push(inner_key.to_s)
-     end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
     end
-   end
-  #binding.pry
+  end
+  new_hash
+  binding.pry
+  p 
 end
